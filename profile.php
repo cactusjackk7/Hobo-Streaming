2ed343970041d78fc2571052d61601aef4cc7450
cactusjackk7 <cactusjackk7@gmail.com>
2022-03-14 17:59:37 +0100

    new page

---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<html lang="nl">
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Settings</title>
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+<p>
+    <a href="History.php" class="btn btn-danger ml-3">History</a>
+</p>
+</body>
+</html>
