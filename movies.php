4ad29b11aba1de477dd177f96aa84ada3209863f
cactusjackk7 <cactusjackk7@gmail.com>
2022-03-14 19:37:14 +0100

    movies page

--- movies.php
+++ movies.php
@@ -4,10 +4,15 @@
 <html lang="nl">
 <head>
     <title>Home</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+<div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#Trending">Trending</a>
+    <a href="profile.php">Profile</a>
+    <input type="text" placeholder="Search..">
+</div>
 </body>
 </html>
 
