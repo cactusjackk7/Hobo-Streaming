b99d82bb5aebfab7fb34160e02fb41fe30d8a6fb
cactusjackk7 <cactusjackk7@gmail.com>
2022-03-21 18:45:26 +0100

    front-end done

--- movies.php
+++ movies.php
@@ -4,16 +4,118 @@
 <html lang="nl">
 <head>
     <title>Home</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="movies.css">
 </head>
 <body>
+<!-- nav -->
 <div class="topnav">
     <a class="active" href="#home">Home</a>
     <a href="trending.php">Trending</a>
     <a href="profile.php">Profile</a>
     <input type="text" placeholder="Search..">
     <a href="logout.php">Log Out</a>
+
+<!-- header -->
+<header class="banner">
+    <div class="banner__contents">
+        <h1 class="banner__title">Money Heist</h1>
+
+        <h1 class="banner__description">
+            To carry out the biggest heist in history, a mysterious man called The Professor recruits
+            a band of eight robbers who have a single characteristic: n...
+        </h1>
+    </div>
+    <div class="banner--fadeBottom"></div>
+</header>
+
+<div class="row">
+    <h2>HoBo Originals</h2>
+    <div class="row__posters">
+        <img class="row__poster row__posterLarge" src="images/large-movie1.jpg" alt="" />
+
+        <img class="row__poster row__posterLarge" src="images/large-movie2.jpg" alt="" />
+
+        <img class="row__poster row__posterLarge" src="images/large-movie3.jpg" alt="" />
+
+        <img class="row__poster row__posterLarge" src="images/large-movie4.jpg" alt="" />
+
+        <img class="row__poster row__posterLarge" src="images/large-movie5.jpg" alt="" />
+
+        <img class="row__poster row__posterLarge" src="images/large-movie6.jpg" alt="" />
+
+        <img class="row__poster row__posterLarge" src="images/large-movie7.jpg" alt="" />
+
+        <img class="row__poster row__posterLarge" src="images/large-movie8.jpg" alt="" />
+    </div>
 </div>
+
+<!-- Trending Now -->
+<div class="row">
+    <h2>Trending Now</h2>
+    <div class="row__posters">
+        <img class="row__poster" src="images/small-movie1.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie2.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie3.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie4.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie5.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie6.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie7.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie8.jpg" alt="" />
+    </div>
+</div>
+
+<!-- Top Rated -->
+<div class="row">
+    <h2>Top Rated</h2>
+    <div class="row__posters">
+        <img class="row__poster" src="images/small-movie1.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie2.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie3.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie4.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie5.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie6.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie7.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie8.jpg" alt="" />
+    </div>
+</div>
+
+<!-- Action Movies -->
+<div class="row">
+    <h2>Action Movies</h2>
+    <div class="row__posters">
+        <img class="row__poster" src="images/small-movie1.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie2.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie3.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie4.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie5.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie6.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie7.jpg" alt="" />
+
+        <img class="row__poster" src="images/small-movie8.jpg" alt="" />
+    </div>
+</div>
+
+
 </body>
 </html>
 
